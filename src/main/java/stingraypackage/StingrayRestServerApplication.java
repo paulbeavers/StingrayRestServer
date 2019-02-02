cbda839f900be28bdeba4209c8a03f0b3a886d94
Paul Beavers <paul_beavers@mac.com>
2019-02-02 13:17:43 -0800

    First commit

---
+++ src/main/java/stingraypackage/StingrayRestServerApplication.java
@@ -0,0 +1,14 @@
+package stingraypackage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StingrayRestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StingrayRestServerApplication.class, args);
+	}
+
+}
+
