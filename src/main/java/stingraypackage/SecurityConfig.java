cd6df62b96619444d6a5299bfc7d6c5d8a1c6089
pbeavers <pbeavers@speedy.hsd1.ma.comcast.net>
2019-02-03 18:42:09 -0600

    Fixed authentication.

--- src/main/java/stingraypackage/SecurityConfig.java
+++ src/main/java/stingraypackage/SecurityConfig.java
@@ -6,28 +6,34 @@ import org.springframework.security.config.annotation.authentication.configurers
 import org.springframework.security.config.annotation.authentication.configurers.provisioning.UserDetailsManagerConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	// Authentication : User --> Roles
 	
-	protected void configure(AuthenticationManagerBuilder auth)
-			throws Exception {
-		
-		
-		UserDetailsManagerConfigurer<AuthenticationManagerBuilder, 
-		InMemoryUserDetailsManagerConfigurer<AuthenticationManagerBuilder>>.UserDetailsBuilder roles = 
-		auth.inMemoryAuthentication().passwordEncoder(org.springframework.security.crypto.password.NoOpPasswordEncoder.getInstance())
-					.withUser("user1")
-					.password("secret1")
-					.roles("USER").and().withUser("admin1").password("secret1")
-					.roles("USER", "ADMIN");
+	
+	@Override
+	
+	protected void configure(AuthenticationManagerBuilder auth) 
+		      throws Exception {
+		        
+		PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();        
+		String userName = "User1";
+		        
+		auth.inMemoryAuthentication()        
+			.withUser(userName)
+    		.password(encoder.encode("secret"))
+	  		.roles("USER");	       
 	}
 	
+	
+	@Override
 	// Authorization : Role -> Access
 	protected void configure(HttpSecurity http) throws Exception {
-		http.httpBasic().and().authorizeRequests().antMatchers("/students/**")
+		http.httpBasic().and().authorizeRequests().antMatchers("/status/**")
 				.hasRole("USER").antMatchers("/**").hasRole("ADMIN").and()
 				.csrf().disable().headers().frameOptions().disable();
 	}
